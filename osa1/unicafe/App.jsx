f1462a2eeb686ee3acaaa8e1061fb5712861ab0a
LiisaKotilainen <liisa.kotilainen@edu.turkuamk.fi>
2024-12-09 17:49:55 +0200

    1.7 unicafe step2

--- osa1/unicafe/App.jsx
+++ osa1/unicafe/App.jsx
@@ -1,23 +1,47 @@
 import { useState } from 'react'
 
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const [total, setTotal] = useState(0)
+  const average = total === 0 ? 0 : (good - bad) / total
+  const posPercentage = total === 0 ? 0 : (good / total ) * 100
+
+  const handleGood = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    setTotal(updatedGood + neutral + bad)
+  }
+
+  const handleNeutral = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    setTotal(updatedNeutral + good + bad)
+  }
+
+  const handleBad = () => {
+    const updatedBad = bad + 1 
+    setBad(updatedBad)
+    setTotal(updatedBad + neutral + good)
+  }
 
   return (
     <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
       <h1>Unicafe Palautesovellus</h1>
       <div>
-        <button onClick={() => setGood(good + 1)}>Hyvä</button>
-        <button onClick={() => setNeutral(neutral + 1)}>Neutraali</button>
-        <button onClick={() => setBad(bad + 1)}>Huono</button>
+        <button onClick={handleGood}>Hyvä</button>
+        <button onClick={handleNeutral}>Neutraali</button>
+        <button onClick={handleBad}>Huono</button>
       </div>
       <h2>Palautteet</h2>
       <p>Hyvä: {good}</p>
       <p>Neutraali: {neutral}</p>
       <p>Huono: {bad}</p>
+      <p>Kaikki: {total}</p>
+      <p>Keskiarvo: {average}</p>
+      <p>Positiivisia: {posPercentage}%</p>
     </div>
   )
 }
