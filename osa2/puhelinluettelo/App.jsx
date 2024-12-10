662d17376da12265582cad21ba1a64d5baabd9b2
LiisaKotilainen <liisa.kotilainen@edu.turkuamk.fi>
2024-12-10 13:50:44 +0200

    2.8 puhelinluettelo step3

--- osa2/puhelinluettelo/App.jsx
+++ osa2/puhelinluettelo/App.jsx
@@ -2,25 +2,33 @@ import { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040 123 1244' }
   ]); 
-  const [newName, setNewName] = useState('');
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value);
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   };
 
   const addPerson = (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
     // Nimen tarkistus
     if (persons.some(person => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`);
+      alert(`${newName} is already added to phonebook`)
       return;
     }
-    const newPerson = { name: newName };
-    setPersons(persons.concat(newPerson));
-    setNewName(''); 
+
+    const newPerson = { name: newName, number: newNumber }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+    setNewNumber('')
   };
 
   return (
@@ -31,17 +39,22 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map(person => (
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}>
+            {person.name} {person.number}
+          </li>
         ))}
       </ul>
-
-      <div>debug: {newName}</div>
+      {/* Debug-tiedot */}
+      <div>debug: {newName}, {newNumber}</div>
     </div>
   );
 };
